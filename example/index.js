0bea0b0ffdd583fd7d812f7549b3ea6ac8b22264
Maykonn <maykonn@outlook.com>
2018-10-18 11:51:58 -0300

    Improved example to implement a readable api

--- example/index.js
+++ example/index.js
@@ -1,30 +1,42 @@
 const Process = require('../src/GlobalProcess.js');
 
+/**
+ * This function will be called on the process queue start
+ */
 const initializer = () => {
   console.log('The global process initializer');
 };
 
+/**
+ * This function will be called when all process on the queue is executed
+ */
 const finisher = () => {
   console.log('The global process finisher');
 };
 
-const asyncFunc = async () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('This is an async function');
-      resolve('foo');
-    }, 1000);
-  });
+/**
+ * Example of an e-commerce cart flow
+ *
+ * @type {{CheckCartItems: function(), CalculateTotal: function(), UpdateStock: function()}}
+ */
+const CartFlow = {
+  CheckCartItems: async () => {
+    return new Promise((resolve) => setTimeout(resolve, 500));
+  },
+  CalculateTotal: async () => {
+    return new Promise((resolve) => setTimeout(resolve, 1000));
+  },
+  UpdateStock: () => {
+    console.log('This is a normal function');
+  }
 };
 
-const normalFunc = () => {
-  console.log('This is a normal function');
-};
-
-const SystemProcess = new Process(initializer, finisher);
-SystemProcess.add(asyncFunc, Process.Await, 1);
-SystemProcess.add(normalFunc, Process.Async, 3);
-SystemProcess.add(asyncFunc, Process.Async, 4);
-SystemProcess.add(normalFunc, Process.Await, 2);
-SystemProcess.exec();
+/**
+ * Example of a cart process
+ */
+const CartProcess = new Process.Handler(initializer, finisher);
+CartProcess.add(CartFlow.CheckCartItems, Process.TypeAwait, 1);
+CartProcess.add(CartFlow.UpdateStock, Process.TypeAsync, 3);
+CartProcess.add(CartFlow.CalculateTotal, Process.TypeAwait, 2);
+CartProcess.exec();
 
