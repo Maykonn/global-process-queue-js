12b3b17d01b2120e6d24f515db55bf4c1d8b2988
Maykonn <maykonn@outlook.com>
2018-10-18 13:30:27 -0300

    Improved task abstraction

--- src/Task.js
+++ src/Task.js
@@ -5,31 +5,87 @@ const ASYNC = 'ASYNC';
 class Task {
 
   constructor() {
+    /**
+     * Task ID
+     *
+     * @type {Number|null}
+     * @private
+     */
     this._id = null;
+
+    /**
+     * Task value
+     *
+     * @type {Function|null}
+     * @private
+     */
     this._value = null;
+
+    /**
+     * Task type
+     *
+     * @type {string}
+     * @private
+     */
     this._type = ASYNC;
   }
 
+  /**
+   * Retrieves the Task ID
+   *
+   * @return {Number}
+   */
   get id() {
     return this._id;
   }
 
+  /**
+   * Retrieves the task value
+   *
+   * @return {Function}
+   */
   get value() {
     return this._value;
   }
 
+  /**
+   * Configures the task value
+   *
+   * @param {Function} value
+   */
   set value(value) {
+    this.isFunction(value);
     this._id = hash(value);
     this._value = value;
   }
 
+  /**
+   * Retrieves the Task type
+   *
+   * @return {string}
+   */
   get type() {
     return this._type;
   }
 
+  /**
+   * Configures the Task type
+   *
+   * @param {string} value
+   */
   set type(value) {
     this._validateType(value);
     this._type = value;
+  }
+
+  /**
+   * Verify if the func param is a function
+   *
+   * @param {Function} func
+   * @return {boolean}
+   */
+  static isFunction(func) {
+    return typeof func === 'function';
   }
 
   /**
