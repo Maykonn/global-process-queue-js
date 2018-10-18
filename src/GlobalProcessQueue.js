e11ae874520c153b48daf6d199f9636f3af3673c
Maykonn <maykonn@outlook.com>
2018-10-18 17:04:59 -0300

    Refactored

--- src/GlobalProcessQueue.js
+++ src/GlobalProcessQueue.js
@@ -1,13 +1,41 @@
+const Task = require('./Task.js');
+
 class GlobalProcessQueue {
+  /**
+   * GlobalProcessQueue constructor
+   */
   constructor() {
+    /**
+     * The process queue
+     *
+     * @type {Array}
+     * @private
+     */
     this._queue = [];
   }
 
-  add(process) {
-    this._queue.push(process);
+  /**
+   * Add a operation as a Task on queue
+   *
+   * @param {Function} operation
+   * @param {string|null} type
+   * @param {number|null} sequence
+   * @return {number}
+   */
+  add(operation, type = null, sequence = null) {
+    if (typeof operation === 'undefined') {
+      throw new Error('The operation param is required');
+    }
+
+    // TODO: Put the task on queue obeying the sequence
+    return this._queue.push(new Task.API(operation, type));
   }
 
-  execute() {
+  del(process) {
+
+  }
+
+  process() {
 
   }
 }
