de9e6d0f69209c58d009877d035527bf8213ef7d
Maykonn <maykonn@outlook.com>
2018-10-19 18:31:15 -0300

    Now is possible to remove task from queue

--- src/GlobalProcessQueue.js
+++ src/GlobalProcessQueue.js
@@ -20,7 +20,7 @@ class GlobalProcessQueue {
    * @param {Function} operation
    * @param {string|null} type
    * @param {number|null} sequence
-   * @return {number}
+   * @return {Task}
    */
   add(operation, type = null, sequence = null) {
     if (typeof operation === 'undefined') {
@@ -28,15 +28,35 @@ class GlobalProcessQueue {
     }
 
     // TODO: Put the task on queue obeying the sequence
-    return this._queue.push(new Task.API(operation, type));
+    const TaskOperation = new Task.API(operation, type);
+    this._queue[sequence] = TaskOperation;
+
+    return TaskOperation;
   }
 
-  del(process) {
+  /**
+   * Remove a Task from the queue
+   *
+   * @param {Task} TaskToRemove
+   */
+  del(TaskToRemove) {
+    if (TaskToRemove instanceof Task.API) {
+      this._queue = this._queue.filter(Task => Task.hash !== TaskToRemove.hash);
+      return true;
+    }
 
+    throw new Error('TaskToRemove param must be an instance of Task.API');
   }
 
   process() {
+    // Removing the falsy elements from the queue (for example: empty items may
+    // occur on the queue when the is pushed into the queue sequences
+    // like 0,2,3 where here the position 1 is an empty item on queue)
+    const queue = this._queue.filter(Boolean);
 
+    // implementing...
+
+    return true;
   }
 }
 
